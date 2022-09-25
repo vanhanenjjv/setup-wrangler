73c55d6b6854ef37522178a7caedf48cf8dd730d
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-09-25 22:25:55 +0300

    asd

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,8 @@ import { saveCache, restoreCache } from '@actions/cache'
     
     if (key && !version) await exec('npm i -g wrangler')
     else await exec(`npm i -g "wrangler@${version}"`)
+
+    await exec('whereis wrangler')
     
     if (!key) await saveCache(['/usr/local/bin/wrangler'], `wrangler-${version}`)
   } catch (error: any) {
