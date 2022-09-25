3692856e2920d4c8b359028643fb5cde1b3fe549
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-09-25 23:23:26 +0300

    asx

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { setFailed, exportVariable, getInput } from '@actions/core'
-import { exec } from '@actions/exec'
+import { exec, getExecOutput } from '@actions/exec'
 import { saveCache, restoreCache } from '@actions/cache'
 
 (async () => {
@@ -14,7 +14,9 @@ import { saveCache, restoreCache } from '@actions/cache'
     
     const version = getInput('wranglerVersion')
 
-    const key = await restoreCache(['/usr/local/bin/wrangler'], `wrangler-${version}`)
+    const lib = await (await getExecOutput('npm bin -g')).stdout
+
+    const key = await restoreCache([`${lib}/wrangler`], `wrangler-${version}`)
     
     if (key && !version) await exec('npm i -g wrangler')
     else await exec(`npm i -g "wrangler@${version}"`)
@@ -22,7 +24,7 @@ import { saveCache, restoreCache } from '@actions/cache'
     await exec('echo $HOME')
     await exec('whereis wrangler')
     
-    if (!key) await saveCache(['/usr/local/bin/wrangler'], `wrangler-${version}`)
+    if (!key) await saveCache([`${lib}/wrangler`], `wrangler-${version}`)
   } catch (error: any) {
     setFailed(error)
   }
