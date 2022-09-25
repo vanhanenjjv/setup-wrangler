23de85a1d8a0f0d681b609c7eebadb1102e95ee8
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-09-25 21:22:48 +0300

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import { setFailed, exportVariable, getInput } from '@actions/core'
-import { mkdirP } from '@actions/io'
 import { exec } from '@actions/exec'
 
 (async () => {
   try {
     const apiToken = getInput('apiToken')
     exportVariable('CLOUDFLARE_API_TOKEN', apiToken)
-
     const accountId = getInput('accountId')
     exportVariable('CLOUDFLARE_ACCOUNT_ID', accountId)
-
     exportVariable('API_CREDENTIALS', 'API Token')
-
-    await mkdirP('./github/workspace/.wrangler')
-  
     const version = getInput('wranglerVersion')
     if (!version) await exec('npm i -g wrangler')
     else await exec(`npm i -g "wrangler@${version}"`)
